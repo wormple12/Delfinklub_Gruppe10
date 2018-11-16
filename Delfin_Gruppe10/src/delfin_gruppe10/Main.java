3674c055f0de8160c8277116556bf3f58f5645ce
wormple12 <wormple12@hotmail.com>
2018-11-16 11:48:00 +0100

    filerHandler with Nio - success!

--- Delfin_Gruppe10/src/delfin_gruppe10/Main.java
+++ Delfin_Gruppe10/src/delfin_gruppe10/Main.java
@@ -6,8 +6,13 @@
 package delfin_gruppe10;
 
 import delfin_gruppe10.data.FileHandler;
+import delfin_gruppe10.data.FileHandlerV2;
 import delfin_gruppe10.domainlogic.Member;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  *
@@ -19,23 +24,19 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        FileHandler fH = new FileHandler("member.txt", "competitive.txt");
+        FileHandlerV2 fH = new FileHandlerV2("member.txt", "competitive.txt");
         Member member = new Member("Jack McDonalds",
                 "01-01-1955",
                 "Very Derp Street 333", "1111", "Long Way From Here",
                 "11112222", "derp@isDerp.derp", true);
-        
+
         Member member3 = new Member("Simon Norup", "20-12-1995", "Lyngevej 39", "3660", "Stenløse", "60893899", "wormple12@hotmail.com", false);
-        
+
         Member member2 = null;
-        
+
         fH.writeMemberToFile(member);
         fH.writeMemberToFile(member3);
-        //member2 = 
-        //ArrayList<Member> members = fH.readMembersFromFile();
-        System.out.println(fH.readMembersFromFile().get(1).getName());
-        System.out.println(fH.readMembersFromFile().get(0).getName());
-        //System.out.println(member2.getName());
-        //System.out.println(fH.readMembersFromFile().get(0).getName());
-    }   
+        System.out.println(fH.readMembersFromFile().get(1));
+        System.out.println(fH.readMembersFromFile().get(0));
+    }
 }
---
+++ Delfin_Gruppe10/src/delfin_gruppe10/data/FileHandlerV2.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package delfin_gruppe10.data;
+
+import delfin_gruppe10.domainlogic.CompetetiveSwimmer;
+import delfin_gruppe10.domainlogic.Member;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author PC 2 2016 SDC-privat
+ */
+public class FileHandlerV2 implements FileHandlerInterface {
+
+    private final String memberPath;
+    private final String competetivePath;
+
+    private static Path FILE;
+
+    public FileHandlerV2(String memberPath, String competetivePath) {
+        this.memberPath = memberPath;
+        this.competetivePath = competetivePath;
+        FILE = Paths.get(memberPath);
+    }
+
+    private List<String> readFile() {
+        // read file and place lines in list
+        try {
+            return Files.readAllLines(FILE);
+        } catch (IOException ex) {
+            try {
+                Files.createFile(FILE);
+                return readFile();
+            } catch (IOException e) {
+                ex.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void writeMemberToFile(Member member) {
+        try {
+            List<String> strings = readFile();
+            strings.add(member.toString());
+            Files.write(FILE, strings);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+
+        }
+    }
+
+    @Override
+    public ArrayList<Member> readMembersFromFile() {
+        ArrayList<Member> members = new ArrayList<>();
+        List<String> strings = readFile();
+        try {
+            for (String string : strings) {
+                String[] vars = new String[8];
+                int endIndex = 0;
+                int startIndex;
+                for (int i = 0; i < vars.length; i++) {
+                    startIndex = string.indexOf("=", endIndex);
+                    endIndex = string.indexOf(",", startIndex);
+                    vars[i] = string.substring(startIndex + 1, endIndex);
+                }
+                LocalDate date = LocalDate.parse(vars[1]);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-YYYY");
+                vars[1] = formatter.format(date);
+
+                members.add(new Member(vars[0], vars[1], vars[2], vars[3], vars[4], vars[5], vars[6], Boolean.parseBoolean(vars[7])));
+            }
+            return members;
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Animals.txt is not formatted properly.");
+            return null;
+        }
+    }
+
+    @Override
+    public void editMemberInFile(String originalName, Member updated) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteMemberInFile(Member member) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Member> readMembersInArrearsFromFile() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void writeCompetetiveToFile(CompetetiveSwimmer swimmer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<CompetetiveSwimmer> readCompetetivesFromFile() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void editCompetetiveInFile(CompetetiveSwimmer swimmer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
