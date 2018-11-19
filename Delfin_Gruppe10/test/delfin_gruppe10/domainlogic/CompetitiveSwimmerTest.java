6b7545cc1c4a5751c6a6b1e1af36d0489605cf79
Ludvig <Ludvig@10.50.137.94>
2018-11-19 21:03:44 +0100

    arbejdede lidt på competetive swimme test klassen

--- Delfin_Gruppe10/test/delfin_gruppe10/domainlogic/CompetitiveSwimmerTest.java
+++ Delfin_Gruppe10/test/delfin_gruppe10/domainlogic/CompetitiveSwimmerTest.java
@@ -5,6 +5,9 @@
  */
 package delfin_gruppe10.domainlogic;
 
+import static delfin_gruppe10.domainlogic.Discipline.BUTTERFLY;
+import static delfin_gruppe10.domainlogic.Discipline.CRAWL;
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,16 +20,24 @@ import static org.junit.Assert.*;
  * @author Ludvig
  */
 public class CompetitiveSwimmerTest {
-       
+    
+    public CompetitiveSwimmerTest() {
+    }
+    
     CompetetiveSwimmer compSwi = new CompetetiveSwimmer("Jack McDonalds",
                 "01-01-1955",
                 "Very Derp Street 333", "1111", "Long Way From Here",
                 "11112222", "derp@isDerp.derp");
     
-    comSwi.setBestTrainingResult(TrainingResult result);
+    TrainingResult traiResBut1 = new TrainingResult(BUTTERFLY, "01:01:01", "04-05-1997");
+    TrainingResult traiResBut2 = new TrainingResult(BUTTERFLY, "02:02:02", "04-05-1997");
+    TrainingResult traiResCra = new TrainingResult(CRAWL, "02:02:02", "04-05-1997");
     
-    public CompetitiveSwimmerTest() {
-    }
+    CompetetiveResult compRes1 = new CompetetiveResult(BUTTERFLY, "03:03:03", "07-08-09", "Lyngby", 1);
+    CompetetiveResult compRes2 = new CompetetiveResult(CRAWL, "03:03:03", "07-08-09", "Lyngby", 2);
+    CompetetiveResult compRes3 = new CompetetiveResult(BUTTERFLY, "01:02:03", "07-08-11", "Holte", 3);
+    
+    ArrayList<CompetetiveResult> cRList = new ArrayList();
     
     @BeforeClass
     public static void setUpClass() {
@@ -38,6 +49,15 @@ public class CompetitiveSwimmerTest {
     
     @Before
     public void setUp() {
+        compSwi.setBestTrainingResult(traiResBut2);
+        compSwi.setBestTrainingResult(traiResCra);
+        compSwi.addCompetetiveResult(compRes1);
+        compSwi.addCompetetiveResult(compRes2);
+        compSwi.addCompetetiveResult(compRes3);
+        
+        cRList.add(compRes1);
+        cRList.add(compRes2);
+        cRList.add(compRes3);
     }
     
     @After
@@ -48,7 +68,12 @@ public class CompetitiveSwimmerTest {
     // The methods must be annotated with annotation @Test. For example:
     //
     @Test
-    public void getBestTrainingResult(Discipline d){
-        assertEquals();
+    public void getBestTrainingResultTest(){      
+        assertEquals(compSwi.getBestTrainingResult(BUTTERFLY), traiResBut2);
     } 
+    
+//    @Test
+//    public void getCompetetiveResultsTest(){
+//        assertEquals(compSwi.getCompetetiveResults(), cRList);
+//    }
 }
